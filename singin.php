2227566dbf17e1413d778746ff8c211a5ea43466
be-wing <sin.hanari@gmail.com>
2024-12-03 02:32:53 +0900

    サインイン 入力チェック追加

--- singin.php
+++ singin.php
@@ -1,7 +1,6 @@
 <?php
 include_once(__DIR__ . '/model/functions.php');
 
-
 if($_POST){
     $user = new userSystem();
     if( $user->userEmailCheck($_POST['userEmail']) ){
@@ -12,10 +11,13 @@ if($_POST){
         $userName = $_POST['userName'];
         $userEmail = $_POST['userEmail'];
         $password = $_POST['userPassword'];
-        $user->setUser($userName,$userEmail,$password);
-
-        //TOPに遷移
-        header("Location: {$const(DIR)}");
+        if($userName != '' && $userEmail != '' && $password !=''){
+            $user->setUser($userName,$userEmail,$password);
+            //TOPに遷移
+            header("Location: {$const(DIR)}");
+        }else{
+            $error[] = '入力値に問題があります';
+        }
     }
 }
 include_once(__DIR__ . '/view/sing-view.php');
